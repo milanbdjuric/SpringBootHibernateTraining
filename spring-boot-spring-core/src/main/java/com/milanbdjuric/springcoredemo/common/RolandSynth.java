aca545d68a5386fa539ef13b38d62139cdbc6e4f
milanbdjuric <sremusch@gmail.com>
2023-07-27 22:28:30 +0200

    lazy initialization demo

--- spring-boot-spring-core/src/main/java/com/milanbdjuric/springcoredemo/common/RolandSynth.java
+++ spring-boot-spring-core/src/main/java/com/milanbdjuric/springcoredemo/common/RolandSynth.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
-@Primary                                         // we can only mark 1 primary class annotation, but @Qualifier comes 1st; without @QUalifier - this would be implemented
+@Primary                                          // we can only mark 1 primary class annotation, but @Qualifier comes 1st; without @QUalifier - this would be implemented
 public class RolandSynth implements Synth{
 
     public RolandSynth(){
