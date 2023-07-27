fa24f43a21b42f3d19714335d9d03681b2bba9f6
milanbdjuric <sremusch@gmail.com>
2023-07-27 21:44:30 +0200

    @Qualifier demo

--- spring-boot-spring-core/src/main/java/com/milanbdjuric/springcoredemo/common/RolandSynth.java
+++ spring-boot-spring-core/src/main/java/com/milanbdjuric/springcoredemo/common/RolandSynth.java
@@ -3,9 +3,9 @@ package com.milanbdjuric.springcoredemo.common;
 import org.springframework.stereotype.Component;
 
 @Component
-public class MoogSynth implements Synth {
+public class RolandSynth implements Synth{
     @Override
     public String getSineWave() {
-        return "BEEEEEEP! BEEEEEEEP!";
+        return "BOOOOOP! BOOOOOP!";
     }
 }
--- spring-boot-spring-core/src/main/java/com/milanbdjuric/springcoredemo/rest/DemoController.java
+++ spring-boot-spring-core/src/main/java/com/milanbdjuric/springcoredemo/rest/DemoController.java
@@ -3,6 +3,7 @@ package com.milanbdjuric.springcoredemo.rest;
 import com.milanbdjuric.springcoredemo.common.Synth;
 import jakarta.servlet.http.PushBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,18 +11,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoController {
 
     private Synth mySynth;
-/*
+
     @Autowired // constructor injection - optional if u have only 1 constructor
-    public DemoController(Synth theSynth) {
+    public DemoController(@Qualifier("korgSynth") Synth theSynth) {
         mySynth = theSynth;
     }
-*/
+/*
     @Autowired   // setter injection
     public void setMySynth(Synth theSynth){
         mySynth = theSynth;
     }
 
-
+*/
     @GetMapping("/synthsound")
     public String getSynthSound(){
         return mySynth.getSineWave();
