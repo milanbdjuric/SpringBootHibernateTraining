e184645413379d2ad5b92943493253effe983dee
milanbdjuric <milandjuric@github.com>
2023-07-31 00:17:59 +0200

    creating an object

---
+++ spring-boot-hibernate-jpa-crud/README.md
@@ -0,0 +1,1 @@
+Hibernate / JPA basic CRUD operations demo.
--- spring-boot-hibernate-jpa-crud/src/main/java/com/milanbdjuric/cruddemo/CruddemoApplication.java
+++ spring-boot-hibernate-jpa-crud/src/main/java/com/milanbdjuric/cruddemo/CruddemoApplication.java
@@ -30,14 +30,14 @@ public class CruddemoApplication {
 	private void createMultipleStudents(StudentDAO studentDAO) {
 
 		System.out.println("Creating three student objects ...");
-		Student tempStudent1 = new Student("Mirsad", "Habibovic", "mirsad@gmail.com");
-		Student tempStudent2 = new Student("Blagoje", "Cabarkapa", "blagoje@gmail.com");
-		Student tempStudent3 = new Student("Miodrag", "Bataveljic", "batavelja@gmail.com");
+	//	Student tempStudent1 = new Student("Mirsad", "Habibovic", "mirsad@gmail.com");
+	//	Student tempStudent2 = new Student("Blagoje", "Cabarkapa", "blagoje@gmail.com");
+	//	Student tempStudent3 = new Student("Miodrag", "Bataveljic", "batavelja@gmail.com");
 
 		System.out.println("Saving the students ...");
-		studentDAO.save(tempStudent1);
-		studentDAO.save(tempStudent2);
-		studentDAO.save(tempStudent3);
+	//	studentDAO.save(tempStudent1);
+	//	studentDAO.save(tempStudent2);
+	//	studentDAO.save(tempStudent3);
 	}
 
 	private void createStudent(StudentDAO studentDAO) {
