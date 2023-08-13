f80722aac278c76000a909b465e7ae0fd708b710
milanbdjuric <sremusch@gmail.com>
2023-08-13 15:21:45 +0200

    Spring Data REST app.props. demo

--- spring-boot-rest-crud/src/main/java/com/milanbdjuric/cruddemo/dao/SynthRepository.java
+++ spring-boot-rest-crud/src/main/java/com/milanbdjuric/cruddemo/dao/SynthRepository.java
@@ -2,6 +2,9 @@ package com.milanbdjuric.cruddemo.dao;
 
 import com.milanbdjuric.cruddemo.entity.Synth;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path="synthesizers")
 public interface SynthRepository extends JpaRepository<Synth, Integer> {
+
 }
