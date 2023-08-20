13ac3b23ae70ed40d80f5a216e77c96058161399
milanbdjuric <sremusch@gmail.com>
2023-08-20 17:22:38 +0200

    InitBinder and StringTrimmerEditor demo

--- spring-boot-validation/src/main/java/com/milanbdjuric/validationdemo/CustomerController.java
+++ spring-boot-validation/src/main/java/com/milanbdjuric/validationdemo/CustomerController.java
@@ -1,10 +1,13 @@
 package com.milanbdjuric.validationdemo;
 
 import jakarta.validation.Valid;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -30,6 +33,11 @@ public class CustomerController {
         else {
             return "customer-confirmation";
         }
+    }
 
+    @InitBinder
+    public void initBinder(WebDataBinder dataBinder){
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+        dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
     }
 }
